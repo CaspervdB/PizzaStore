e56cdb935828d2da6c122badb32e3da4d9e93b87
RooskenDaniel <43609262+RooskenDaniel@users.noreply.github.com>
2022-04-01 15:45:38 +0200

    removed test and other unnesicery things

--- Pizzawinkel/src/com/nhlstenden/Main.java
+++ Pizzawinkel/src/com/nhlstenden/Main.java
@@ -9,16 +9,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Pizza pizza = new PizzaSalami();
-        pizza = new Ananas(pizza);
-        pizza = new ExtraKaas(pizza);
-        pizza = new KaasInKorst(pizza);
-
-        printPizza(pizza);
-    }
-
-    private static void printPizza(Pizza p)
-    {
-        System.out.println("Cost: " + p.getCost() + ", description: " + p.getDescription());
+        PizzaWinkel martini = new PizzaWinkel();
     }
 }
